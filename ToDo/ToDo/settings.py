506db7ef4702cbd799a45f70fb85009effbf3554
unknown <202280295@psu.palawan.edu.ph>
2024-01-06 12:37:09 +0800

    allowing host

--- ToDo/ToDo/settings.py
+++ ToDo/ToDo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#q8bd8vn2j(8*zyt&gnv*is9#=iybdypski0uzo^ru0m@!7&5j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vinten.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
